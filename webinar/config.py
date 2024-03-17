c72dcb34dd8fc02e9fa42c5734248ef07285ece6
Chen Rothschild <chen.rothschild@tensorleap.ai>
2024-03-17 15:09:27 +0200

    add config files

---
+++ webinar/config.py
@@ -0,0 +1,16 @@
+import os
+from typing import Dict, Any
+import yaml
+
+
+def load_od_config() -> Dict[str, Any]:
+    # Load the existing YAML config
+    root = os.path.abspath(os.path.dirname(__file__))
+    file_path = os.path.join(root, 'config.yml')
+    with open(file_path, 'r') as file:
+        config = yaml.safe_load(file)
+
+    return config
+
+
+CONFIG = load_od_config()
